04d900550f8fd6b5a4677e242aea56247ba7189e
Kamaal <kamaal@tranzer.com>
2021-07-12 08:53:35 +0200

    Adding move and zip files

--- files/files.go
+++ files/files.go
@@ -1,7 +1,10 @@
 package files
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 )
 
@@ -17,4 +20,53 @@ func AppendFileToPath(path string, file string) string {
 	}
 
 	return fmt.Sprintf("%s/%s", path, file)
+}
+
+func Move(fromPath string, destination string) error {
+	return os.Rename(fromPath, destination)
+}
+
+func ZipFiles(filename string, files []string) error {
+	newZipFile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer newZipFile.Close()
+
+	zipWriter := zip.NewWriter(newZipFile)
+	defer zipWriter.Close()
+
+	for _, file := range files {
+		if err = addFileToZip(zipWriter, file); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func addFileToZip(zipWriter *zip.Writer, filename string) error {
+	fileToZip, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer fileToZip.Close()
+
+	info, err := fileToZip.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+	header.Name = filename
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(writer, fileToZip)
+	return err
 }
